4dd402185d74eefe57b9271367bc46cfcc04b38d
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-05-23 20:29:58 +0530

    Add files via upload

---
+++ Euclid_GCD.java
@@ -0,0 +1,23 @@
+package DSA;
+import java.util.Scanner;
+
+public class Euclid_GCD {
+	static int e_gcd(int a,int b){
+		if(b==0) {
+			return a;
+		}
+		else
+			return e_gcd(b,a%b);
+		
+	}
+	public static void main(String args[]) {
+		System.out.println("Enter the value of a:");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println("Enter the value of b:");
+		int b=sc.nextInt();
+		System.out.println("the gcd is :"+e_gcd(a,b));
+		
+	}
+
+}
