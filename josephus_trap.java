3e4817d0de43f21f4b5fdece3eba412265a4ca35
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-05-27 01:01:00 +0530

    Add files via upload

---
+++ josephus_trap.java
@@ -0,0 +1,28 @@
+package myproject;
+
+import java.util.ArrayList;
+
+public class josephus_trap {
+	
+	static void trap(ArrayList<Integer>L,int k,int pos) {
+		if(L.size()==1) {
+			System.out.print(L.get(0));
+			return;
+		}
+		pos=(pos+k)%L.size();
+		L.remove(pos);
+		trap(L,k,pos);
+		
+	}
+	public static void main(String args[]) {
+		int pos=0;
+		int k=1;
+		System.out.print("The alive solder is : ");
+		ArrayList<Integer>L=new ArrayList<Integer>();
+		for(int i=1;i<=100;i++) {
+			L.add(i);
+		}
+		trap(L,k,pos);
+	}
+
+}
