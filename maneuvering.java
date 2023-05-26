0919bb3a0924bdb8f91651795017b30c3f919aa0
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-05-26 16:29:21 +0530

    Add files via upload

---
+++ maneuvering.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class maneuvering {
+	static int row =4;
+	static int col=4;
+	static int path(int r,int c) {
+		if(r==row-1||c==col-1)
+			return 1;
+		return path(r,c+1)+path(r+1,c);
+	}
+	public static void main(String args[]) {
+		System.out.println("the possible way:"+path(0,0));
+	}
+
+}
