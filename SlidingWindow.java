f3dea2bd1ae73aaed6cdb8b5e370f27167b4a58d
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-07-21 07:15:24 +0530

    Add files via upload

---
+++ SlidingWindow.java
@@ -0,0 +1,24 @@
+package myproject;
+
+public class SlidingWindow {
+	static void slide(int arr[],int k) {
+		int n=arr.length;
+		int max=0;
+		for(int i=0;i<=n-k;i++) {
+			max=arr[i];
+			for(int j=1;j<k;j++) {
+				if(arr[i+j]>max) {
+					max=arr[i+j];
+				}
+			}
+			System.out.print(max+" ");
+			
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,1,4,5,2,3,6};
+		slide(arr,3);
+	}
+   }
