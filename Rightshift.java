32d8eeab0f68dbbc04dc707b206f8a846fe5121c
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-07-25 09:08:24 +0530

    Add files via upload

---
+++ Rightshift.java
@@ -0,0 +1,17 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class Rightshift {
+	public static void main (String args[]) {
+	System.out.println("Enter your number:");
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	System.out.println("Enter your number for right shift operation:");
+	int v=sc.nextInt();
+	int b= x>>v;
+	System.out.println("The right shift of "+ x +" is: "+ b);
+	}
+
+
+}
