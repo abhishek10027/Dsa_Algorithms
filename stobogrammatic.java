5000ae709ab5048047c293a52247ecc0b8e1286a
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-07-27 08:06:28 +0530

    Add files via upload

---
+++ stobogrammatic.java
@@ -0,0 +1,45 @@
+package myproject;
+import java.util.*;
+public class stobogrammatic {
+	public static void main(String args[]) {
+		
+		System.out.println("Enter your number:- ");
+		Scanner sc=new Scanner(System.in);
+		String n=sc.nextLine();
+		
+		boolean ans=is_stobogrammatic(n);
+		if(ans==true)
+		
+			System.out.print("it is stobogrammatic number:"+n);
+		else
+			
+			System.out.print("it is not a stobogrammatic number:"+n);
+			
+			
+		
+	}
+
+
+public static boolean is_stobogrammatic(String n){
+	if(n==null||n.length()==0) {
+		return true;
+	}
+	HashMap<Character,Character>map=new HashMap<Character,Character>();
+	map.put('0', '0');
+	map.put('1', '1');
+	map.put('8', '8');
+	map.put('6', '9');
+	map.put('9', '6');
+	int left=0;
+	int right =n.length()-1;
+	while(left<=right) {
+		if(!map.containsKey(n.charAt(right))||n.charAt(left)!=map.get(n.charAt(right))){
+			return false;
+		}
+		left++;
+		right--;
+			
+		}
+		return true;
+	}
+}
