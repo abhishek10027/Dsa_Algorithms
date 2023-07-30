c8dbb1ce056d1e6babf4898eb9cd786e8be7fe22
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-07-30 10:36:49 +0530

    Add files via upload

---
+++ fibonacci_series.java
@@ -0,0 +1,23 @@
+package myproject;
+
+import java.util.*;
+public class fibonacci_series {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your n th term position:");
+		int n=sc.nextInt();
+		int a=0;
+		int b=1;
+		System.out.print(a+" "+b);
+		int c;
+		for (int i=0;i<=n;i++) {
+			c=a+b;
+			a=b;
+			b=c;
+			System.out.print(" "+c);
+			
+		}
+		
+	}
+
+}
