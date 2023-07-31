d7e86cad1549cdec2ecf596ed0706bd671fee861
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-07-31 08:59:36 +0530

    Add files via upload

---
+++ Aliceappletree.java
@@ -0,0 +1,36 @@
+package myproject;
+
+import java .util.Scanner;  
+
+public class Aliceappletree {
+	static int appletree(int n,int s,int e,int w,int m,int k) {
+		if(m<=s*k)
+			return m;
+		else if (m<=s*k+e+w)
+			return (s*k+(m-s*k)*k);
+		else 
+			return -1;
+		
+	}
+public static void main(String args[]) {
+
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the vaule of m :- ");
+	int m=sc.nextInt();
+	System.out.println("enter the vaule of k :- ");
+	int k=sc.nextInt();
+	System.out.println("enter the vaule of n :- ");
+	int n=sc.nextInt();
+	System.out.println("enter the vaule of s :- " );
+	int s=sc.nextInt();
+	System.out.println("enter the vaule of w :- ");
+	int w=sc.nextInt();
+	System.out.println("enter the vaule of e :- ");
+	int e=sc.nextInt();
+	
+	int out=appletree(m,k,n,s,w,e);
+	System.out.println("the answer is : "+out);
+	
+	}
+}
+
