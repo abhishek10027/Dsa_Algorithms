3e80351082d086d06b9f705513cec99ccf90b9db
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-08-02 08:32:57 +0530

    Add files via upload

---
+++ nibble.java
@@ -0,0 +1,19 @@
+package myproject;
+
+import java.util.*;
+
+class nibble{
+ 
+  
+  public static void main(String[] args){	
+	    System.out.println("Enter your number:");
+	    Scanner sc=new Scanner(System.in);
+	    int num=sc.nextInt();
+	    int newnum;
+	    newnum=(((num&0X0F)<<4)|((num&0XF0)>>4));
+	    System.out.println("Before nibble swap:"+num);
+	    System.out.println("\nAfter nibble swap:"+newnum);
+	    
+}
+}
+ 
