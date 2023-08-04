0292317f41dfcf540fc72c31472f6592ba365638
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2023-08-04 17:39:30 +0530

    Add files via upload

---
+++ LeaderInArray.java
@@ -0,0 +1,22 @@
+package myproject;
+
+public class LeaderInArray {
+	public static void findleader(int [] arr) {
+		int n=arr.length;
+		int max=Integer.MIN_VALUE;//iterate the array from right to left
+		for (int i=n-1;i>=0;i--) {
+			if(arr[i]>max) {
+				// if the current elements is greater then the max ,it is a leader
+				System.out.print(arr[i]+" ");
+				max=arr[i];
+				
+			}
+		}
+	}
+	public static void main(String args[]) {
+		int [] arr= {12,5,12,10,12,10,4,8,7,8,1};
+		System.out.println("The leader are :- ");
+		findleader(arr);
+	}
+
+}
